988853be3451bd0ec3bbfaec5ebdda5eadc1d8b1
sutogone <batorukunn@yahoo.co.jp>
2020-10-14 12:33:49 +0900

    ユーザーの情報表示の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
     if current_user.update(user_params) #もし、現在ログインしているユーザー情報がアップデートされたらroot_pathに戻る
       redirect_to root_path             #そうでなければeditのビューに戻る
     else
-      render :edit
+      render :edit                      #renderは指定するビュー直接を呼び出すredirect_toとの違いは後者はルーティング経由で呼び出している
     end
   end
   
