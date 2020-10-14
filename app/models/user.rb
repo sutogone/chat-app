9a8a460f157d45df64fa11badd6af01668e22064
sutogone <batorukunn@yahoo.co.jp>
2020-10-14 14:38:31 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: :true  #バリデーションとはデータ登録の際に一定の制約を設けること
+                                   #validates :カラム名, バリデーションの種類presence :trueは空のカラムを受付ない
 end
